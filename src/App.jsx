f436faf660dd196fc55b478b2f93564ab517c87a
rinu-mary-john-89 <rinu89@gmail.com>
2026-02-07 09:45:15 +0300

    add data and dispaly the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,23 @@
+import { useState } from "react";
+import data from "./data";
+import List from "./List";
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(data);
+
+  return (
+    <main>
+      <section className="container">
+        <h1>{people.length} Birthdays Today</h1>
+        <List people={people}></List>
+        <button
+          type="button"
+          className="btn btn-block"
+          onClick={() => setPeople([])}
+        >
+          clear all
+        </button>
+      </section>
+    </main>
+  );
 };
 export default App;
